812b134469e74ca59b5e7e356f43275dd402840f
boreddad <ronnebaumfinances@gmail.com>
2024-01-03 15:31:21 -0500

    feat: parse bulk strings

--- src/parser.rs
+++ src/parser.rs
@@ -1,208 +1,145 @@
-use crate::{lexer::Lexer, lexi_type::LexiType, token::Token};
-use anyhow::{anyhow, Result};
+use crate::lexi_data::LexiData;
 
-pub struct Parser {
-    l: Lexer,
-    cur: Option<Token>,
-    peek: Option<Token>,
+pub struct Parser<'a> {
+    input: &'a [u8],
+    pos: usize,
+    ch: u8,
 }
 
-impl Parser {
-    pub fn new(l: Lexer) -> Self {
-        let mut p = Parser {
-            l,
-            cur: None,
-            peek: None,
+impl<'a> Parser<'a> {
+    pub fn new(input: &'a [u8]) -> Self {
+        let mut p = Self {
+            input,
+            pos: 0,
+            ch: 0,
         };
-
-        p.next_token();
-        p.next_token();
-
-        p
+        p.read_byte();
+        return p;
     }
 
-    pub fn parse(&mut self) -> Result<LexiType> {
-        match &self.cur {
-            Some(Token::Simple(v)) => {
-                let str: String = v.to_string();
-                if !self.expect_peek(Token::RetCar) {
-                    return Err(anyhow!("peek err"));
-                }
-                if !self.expect_peek(Token::NewL) {
-                    return Err(anyhow!("peek err"));
-                }
-                self.next_token();
-                Ok(LexiType::Simple(str))
-            }
-            Some(Token::BulkType) => {
-                let str: String;
-                self.next_token();
-                match self.cur {
-                    Some(Token::Len(_)) => {}
-                    _ => return Err(anyhow!("peek err")),
-                };
-                if !self.expect_peek(Token::RetCar) {
-                    return Err(anyhow!("peek err"));
-                }
-                if !self.expect_peek(Token::NewL) {
-                    return Err(anyhow!("peek err"));
-                }
-                self.next_token();
-                match &self.cur {
-                    Some(Token::Bulk(v)) => {
-                        str = v.to_string();
-                    }
-                    _ => return Err(anyhow!("expected bulk")),
-                };
-                if !self.expect_peek(Token::RetCar) {
-                    return Err(anyhow!("peek err"));
-                }
-                if !self.expect_peek(Token::NewL) {
-                    return Err(anyhow!("peek err"));
-                }
-                self.next_token();
-                Ok(LexiType::BulkString(str))
-            }
-            Some(Token::ArrayType) => {
-                let mut vec: Vec<LexiType> = Vec::new();
-                self.next_token();
-                match &self.cur {
-                    Some(Token::Len(l)) => {
-                        let len: usize = l.parse()?;
-                        if !self.expect_peek(Token::RetCar) {
-                            return Err(anyhow!("peek err"));
-                        }
-                        if !self.expect_peek(Token::NewL) {
-                            return Err(anyhow!("peek err"));
-                        }
-                        self.next_token();
-                        for _ in 0..len {
-                            let val = self.parse()?;
-                            vec.push(val);
-                        }
-                    }
-                    _ => return Err(anyhow!("peek err")),
-                };
-                Ok(LexiType::Array(vec))
-            }
-            Some(Token::Int(buf)) => {
-                let res: i64;
-                let mut temp: u64 = 0;
-                let mut shift = 56;
-                for b in buf.iter() {
-                    let btmp = *b as u64;
-                    temp |= btmp << shift;
-                    shift -= 8;
-                }
-                // hack for checking if value should be negative
-                if temp <= 0x7fffffffffffffff {
-                    res = temp as i64;
-                } else {
-                    res = -1 - ((0xffffffffffffffff - temp) as i64);
-                }
-                if !self.expect_peek(Token::RetCar) {
-                    return Err(anyhow!("peek err"));
-                }
-                if !self.expect_peek(Token::NewL) {
-                    return Err(anyhow!("peek err"));
-                }
-                self.next_token();
-                Ok(LexiType::Int(res))
-            }
-            Some(Token::Err(e)) => {
-                let s = e.to_string();
-                if !self.expect_peek(Token::RetCar) {
-                    return Err(anyhow!("peek err"));
-                }
-                if !self.expect_peek(Token::NewL) {
-                    return Err(anyhow!("peek err"));
-                }
-                self.next_token();
-                Ok(LexiType::Error(s))
-            }
-            _ => Err(anyhow!("illegal")),
+    pub fn parse(&mut self) -> anyhow::Result<LexiData> {
+        match self.ch {
+            b'$' => self.parse_string(),
+            _ => todo!(),
         }
     }
 
-    fn expect_peek(&mut self, tok: Token) -> bool {
-        if let Some(t) = &self.peek {
-            if tok == *t {
-                self.next_token();
-                return true;
-            }
-            return false;
+    fn parse_string(&mut self) -> anyhow::Result<LexiData> {
+        let mut string = String::new();
+        if !self.expect_peek_to_be_num() {
+            return Err(anyhow::anyhow!("expected length"))
         }
-        false
+        let length = self.parse_length();
+
+        if !self.cur_byte_is(b'\r') {
+            return Err(anyhow::anyhow!("expected retcar"))
+        }
+        if !self.expect_peek(b'\n') {
+            return Err(anyhow::anyhow!("expected newline"))
+        }
+
+        self.read_byte();
+
+        for _ in 0..length {
+            string.push(self.ch as char);
+            self.read_byte();
+        }
+
+        if !self.cur_byte_is(b'\r') {
+            return Err(anyhow::anyhow!("expected retcar"))
+        }
+        if !self.expect_peek(b'\n') {
+            return Err(anyhow::anyhow!("expected newline"))
+        }
+
+        self.read_byte();
+        return Ok(LexiData::Bulk(string));
     }
 
-    fn next_token(&mut self) {
-        self.cur = self.peek.clone();
-        let n = self.l.next_token();
-        match n {
-            Token::Eof => self.peek = None,
-            _ => self.peek = Some(n),
+    fn parse_length(&mut self) -> usize {
+        let mut res: usize = 0;
+        while Parser::is_digit(self.ch) {
+            res = (res * 10) + ((self.ch - b'0') as usize);
+            self.read_byte();
         }
+        return res;
+    }
+
+    fn peek_byte(&self) -> u8 {
+        if self.pos >= self.input.len() {
+            return 0;
+        }
+        return self.input[self.pos];
+    }
+
+    fn cur_byte_is(&self, byte: u8) -> bool {
+        return self.ch == byte;
+    }
+
+    fn peek_byte_is(&self, byte: u8) -> bool {
+        return self.peek_byte() == byte;
+    }
+
+    fn expect_peek(&mut self, byte: u8) -> bool {
+        if self.peek_byte_is(byte) {
+            self.read_byte();
+            return true;
+        }
+        return false;
+    }
+
+    fn expect_peek_to_be_num(&mut self) -> bool {
+        if Parser::is_digit(self.peek_byte()) {
+            self.read_byte();
+            return true;
+        }
+        return false;
+    }
+
+    fn is_digit(ch: u8) -> bool {
+        return b'0' <= ch && ch <= b'9';
+    }
+
+    fn read_byte(&mut self) {
+        if self.pos >= self.input.len() {
+            return;
+        }
+        self.ch = self.input[self.pos];
+        self.pos += 1;
     }
 }
 
 #[cfg(test)]
 mod test {
 
-    use crate::builder::Builder;
-    use crate::lexer::Lexer;
-    use crate::lexi_type::LexiType;
-    use crate::parser::Parser;
+    use crate::lexi_data::LexiData;
 
-    #[test]
-    fn it_can_parse_bulk_strings() {
-        let buf = Builder::new().add_bulk("vince").out();
-        let l = Lexer::new(buf);
-        let mut p = Parser::new(l);
-        let val = p.parse().unwrap();
-        assert_eq!(val, LexiType::BulkString("vince".to_owned()));
+    use super::Parser;
+
+    struct StringTest<'a> {
+        input: &'a [u8],
+        exp: &'static str,
     }
 
     #[test]
-    fn it_can_parse_arrays() {
-        let buf = Builder::new()
-            .add_arr(2)
-            .add_bulk("vince")
-            .add_bulk("is cool")
-            .out();
-        let lexer = Lexer::new(buf);
-        let mut p = Parser::new(lexer);
-        let val = p.parse().unwrap();
-        let exps = vec![
-            LexiType::BulkString("vince".to_owned()),
-            LexiType::BulkString("is cool".to_owned()),
+    fn it_can_parse_strings() -> anyhow::Result<()> {
+        let tests = [
+            StringTest {
+                input: b"$3\r\nfoo\r\n",
+                exp: "foo",
+            },
         ];
-        assert_eq!(val, LexiType::Array(exps));
-    }
 
-    #[test]
-    fn it_can_parse_integers() {
-        let buf = Builder::new().add_int(42069).out();
-        let l = Lexer::new(buf);
-        let mut p = Parser::new(l);
-        let val = p.parse().unwrap();
-        assert_eq!(val, LexiType::Int(42069));
-    }
+        for test in tests {
+            let mut p = Parser::new(test.input);
+            let data = p.parse()?;
+            assert!(matches!(data, LexiData::Bulk(_)));
+            match data {
+                LexiData::Bulk(s) => assert_eq!(test.exp, s),
+                _ => unreachable!(),
+            }
+        }
 
-    #[test]
-    fn it_can_parse_negative_ints() {
-        let buf = Builder::new().add_int(-42069).out();
-        let l = Lexer::new(buf);
-        let mut p = Parser::new(l);
-        let val = p.parse().unwrap();
-        assert_eq!(val, LexiType::Int(-42069));
-    }
-
-    #[test]
-    fn it_can_parse_errors() {
-        let buf = vec![b'-', b'E', b'r', b'r', b'\r', b'\n'];
-        let l = Lexer::new(buf);
-        let mut p = Parser::new(l);
-        let val = p.parse().unwrap();
-        assert_eq!(val, LexiType::Error("Err".to_string()));
+        Ok(())
     }
 }
