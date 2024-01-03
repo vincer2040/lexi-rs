9893e3baf6e826f8e2eb2aece9373dad1ef049df
boreddad <ronnebaumfinances@gmail.com>
2024-01-03 17:01:23 -0500

    feat: add some impl into for LexiData

--- src/lexi_data.rs
+++ src/lexi_data.rs
@@ -13,8 +13,56 @@ pub enum LexiData {
     Array(Vec<LexiData>),
 }
 
+impl Into<LexiData> for &str {
+    fn into(self) -> LexiData {
+        LexiData::Bulk(self.to_string())
+    }
+}
+
+impl Into<LexiData> for String {
+    fn into(self) -> LexiData {
+        LexiData::Bulk(self)
+    }
+}
+
 impl Into<LexiData> for i64 {
     fn into(self) -> LexiData {
         LexiData::Int(self)
+    }
+}
+
+impl Into<LexiData> for i32 {
+    fn into(self) -> LexiData {
+        LexiData::Int(self as i64)
+    }
+}
+
+impl Into<LexiData> for i16 {
+    fn into(self) -> LexiData {
+        LexiData::Int(self as i64)
+    }
+}
+
+impl Into<LexiData> for i8 {
+    fn into(self) -> LexiData {
+        LexiData::Int(self as i64)
+    }
+}
+
+impl Into<LexiData> for u32 {
+    fn into(self) -> LexiData {
+        LexiData::Int(self as i64)
+    }
+}
+
+impl Into<LexiData> for u16 {
+    fn into(self) -> LexiData {
+        LexiData::Int(self as i64)
+    }
+}
+
+impl Into<LexiData> for u8 {
+    fn into(self) -> LexiData {
+        LexiData::Int(self as i64)
     }
 }
